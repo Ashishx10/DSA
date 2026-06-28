1a23f5fbb8172924b0c830bf800480852d0e92ab
Ashish Chidi <ashishchidi.37@gmail.com>
2026-06-28 22:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.08%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -7,9 +7,8 @@ class Solution:
         for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-        for i in range(n):
-            for j in range(n//2):
-                matrix[i][j],matrix[i][n-j-1]=matrix[i][n-j-1],matrix[i][j]
+        for row in matrix:
+            row.reverse()
 #time complexity:o(n^2)
 #space complexity:o(1)
         
