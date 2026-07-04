bcb6ab68889c4f9b3716614a4f58699da4cc308b
Ashish Chidi <ashishchidi.37@gmail.com>
2026-07-04 13:05:10 +0530

    Time: 62 ms (30.12%) | Memory: 29.5 MB (88.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,16 +3,16 @@ class Solution:
         n=len(height)
         l=0
         r=n-1
-        max_area=0
+        max_amount=0
         while l<r:
             w=r-l
             h=min(height[l],height[r])
             a=w*h
-            max_area=max(max_area,a)
+            max_amount=max(max_amount,a)
             if height[l]<height[r]:
                 l+=1
             else:
                 r-=1
-        return max_area
+        return max_amount
 #time complexity:o(n)
 #space complexity:o(1)
