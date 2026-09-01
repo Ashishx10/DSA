ea21f3384721720a470c20576c678eae64956b81
Ashish Chidi <ashishchidi.37@gmail.com>
2026-09-01 21:40:02 +0530

    Time: 2 ms (79.02%) | Memory: 21 MB (31.63%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        order = []
+        g = defaultdict(list)
+        for a,b in prerequisites:
+            g[a].append(b)
+        unvisited = 0
+        visiting = 1
+        visited = 2
+        states = [unvisited]*numCourses
+        def dfs(i):
+            if states[i] == visiting:
+                return False
+            elif states[i] == visited:
+                return True
+            states[i] = visiting
+            for nei in g[i]:
+                if not dfs(nei):
+                    return False
+            states[i] = visited
+            order.append(i)
+            return True
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return order  
+# time complexity:o(N+E
+# space complexity: o(N)         
