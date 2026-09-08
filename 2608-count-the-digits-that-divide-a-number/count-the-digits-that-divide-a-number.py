c405d893ea789ffdf5d01820f8bd0a572a4d2f70
Ashish Chidi <ashishchidi.37@gmail.com>
2026-09-08 21:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.35%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        count = 0
+        for digit_char in str(num):
+            digit = int(digit_char)
+            if num % digit == 0:
+                count += 1
+        return count
+# time complexity: o(n)
+# space complexity: o(1)
