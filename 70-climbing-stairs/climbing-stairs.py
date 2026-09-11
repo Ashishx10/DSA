75ac32236bf16d539375108a31463fa7fd377a96
Ashish Chidi <ashishchidi.37@gmail.com>
2026-09-11 21:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -8,6 +8,8 @@ class Solution:
             return 2
         return self.climbStairs(n-2) + self.climbStairs(n-1)
 '''
+
+'''
 # Top Down (memotization) approach
 class Solution:
     def climbStairs(self, n: int) -> int:
@@ -19,3 +21,33 @@ class Solution:
                 memo[n] = f(n-2) + f(n-1)
                 return memo[n]
         return f(n)
+'''
+
+'''
+# Bottom Up (tabulation) approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0] * n
+        dp[1] = 0
+        dp[2] = 1
+        for i in range(2,n):
+            dp[i] = dp[i-2] + dp[i-1]
+        return dp[n-1]
+'''
+
+# Bottom Up (tabulation) approach with optimisation
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        prev = 1
+        curr = 2
+        for i in range(2,n):
+            prev,curr = curr, prev+curr
+        return curr
