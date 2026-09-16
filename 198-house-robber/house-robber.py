cf3ed74b42ecacdf907ebf046074b7d2ef122d42
Ashish Chidi <ashishchidi.37@gmail.com>
2026-09-16 21:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (25.20%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,29 @@
+'''
+Recursive approach
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        n = len(nums)
+        def helper(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0] , nums[1])
+            return max(nums[i] + helper(i-2), helper(i-1))
+        return helper(n-1)
+'''
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0],nums[1])
+        memo = {0:nums[0], 1:max(nums[0],nums[1])}
+        def helper(i):
+            if i in memo:
+                return memo[i]
+            else:
+                memo[i] = max(nums[i]+ helper(i-2), helper(i-1))
+                return memo[i]
+        return helper(n-1)
+
