f55364f873b559e72bad00828323a79cad6cac21
Ashish Chidi <ashishchidi.37@gmail.com>
2026-09-17 22:39:26 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (7.94%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,28 @@
+# recursive approach
+'''
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def paths (i,j):
+            if i == j == 0:
+                return 1
+            elif i < 0 or j < 0 or i == m or j == n:
+                return 0
+            else:
+                return paths(i,j-1) + paths(i-1,j)
+        return paths(m-1,n-1)
+'''
+# top down approach(memotization)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {(0,0):1}
+        def paths (i,j):
+            if(i,j) in memo:
+                return memo[(i,j)]
+            elif i < 0 or j < 0 or i == m or j == n:
+                return 0
+            else:
+                val = paths(i,j-1) + paths(i-1,j)
+                memo[(i,j)] = val
+                return val
+        return paths(m-1,n-1)
+
