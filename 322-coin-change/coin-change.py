1e60b61abd6af806b554a68da12464c919b0332d
Ashish Chidi <ashishchidi.37@gmail.com>
2026-09-18 22:15:14 +0530

    Time: 475 ms (69.09%) | Memory: 19.6 MB (78.63%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,3 +1,5 @@
+# Top down approach(memotization)
+'''
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
         coins.sort()
@@ -18,3 +20,23 @@ class Solution:
             return result
         else:
             return -1
+'''
+# Bottom up approach(tabulation)
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        coins.sort()
+        dp = [0] * (amount+1)
+        for i in range(1,amount + 1):
+            minn = float('inf')
+            for coin in coins:
+                diff = i - coin
+                if diff < 0:
+                    break
+                minn = min(minn, dp[diff] + 1 )
+            dp[i] = minn
+        if dp[amount] < float('inf'):
+            return dp[amount]
+        else:
+            return -1
+
+            
